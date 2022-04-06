e5a8d9e932d0db350c10ba3300d5024a0ea00c4c
jorge ochipinti <jorgeochipinti97@gmail.com>
2022-04-06 00:45:22 -0300

    footer2

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -2,22 +2,29 @@ import Link from "next/link"
 
 export const Footer = () => {
     return (
-        <footer className="  mb-8 pt-7 bg-black rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
-            <div className="flex justify-center">
-
-                <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="#" className="hover:underline">Lazaro</a>. All Rights Reserved.
-                </span>
-            </div>
-            <ul className="flex flex-wrap justify-center items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+        <footer className="pb-3 pt-7 bg-black rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 ">
+            <ul className="mb-4 flex flex-wrap justify-center items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
                 <li>
                     <Link href="/About" passHref>
                         <a className="mr-4 hover:underline md:mr-6 ">About Us</a>
                     </Link>
                 </li>
-                <Link href="/Contact" passHref>
-                    <a className="mr-4  hover:underline md:mr-6 ">Contact</a>
-                </Link>
+                <li>
+                    <Link href="/" passHref>
+                        <a className="mr-4  hover:underline md:mr-6 ">Home</a>
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/Contact" passHref>
+                        <a className="mr-4  hover:underline md:mr-6 ">Contact</a>
+                    </Link>
+                </li>
             </ul>
+            <div className="flex justify-center">
+
+                <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="#" className="hover:underline">Lazaro</a>. All Rights Reserved.
+                </span>
+            </div>
         </footer>
     )
 }
